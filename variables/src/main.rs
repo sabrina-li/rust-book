774590da4182e8a20cec5a76fd4cd0d48b0fa37e
Sabrina Li <sabrinali0312@gmail.com>
2020-04-22 12:46:38 -0400

    after 3.1

---
+++ variables/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    // let mut x = 5;
+    // println!("The value of x is {}", x);
+    // x = 6;
+    // println!("The value of x is {}", x);
+
+    // const MAX_POINTS: i32 = 100_000;
+    // println!("The max point is {}", MAX_POINTS);
+
+    // let x = 5;
+    // let x = x + 1;
+    // let x = x * 2;
+    // let x = 10;
+    // println!("The value of x is: {}", x);
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("spaces is {}", spaces);
+
+    let mut spaces = "   ";
+    spaces = spaces.len(); //mismatched types
+}
